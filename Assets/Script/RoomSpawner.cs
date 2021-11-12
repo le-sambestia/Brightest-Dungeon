5ebe7f99cdeee035f5dca0a4dbc3fdc8352870af
le_sambestia <515985@chrome.escg.ac.uk>
2021-11-12 16:17:35 +0000

    added random generation and room spawning but all rooms are offset. had problem with infinitely spawning rooms

--- Assets/Script/RoomSpawner.cs
+++ Assets/Script/RoomSpawner.cs
@@ -5,28 +5,59 @@ using UnityEngine;
 public class RoomSpawner : MonoBehaviour
 {
     public int openingDirection;
-    //1 = need bottom room
-    //2 = need top room
-    //3 = need left room
-    //4 = need right room
+    //1 = top room
+    //2 = bottom room
+    //3 = right room
+    //4 = left room
 
-    void Update()
+    private RoomTemplates templates;
+    private int rand;
+    private bool spawned = false;
+
+    void Start()
     {
-        switch(openingDirection)
+        templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+        Invoke("Spawn", 2f);
+    }
+
+    void Spawn()
+    {
+        if(spawned== false)
         {
-            case 1:
-                //spawn door with bottom entrance
-                break;
-            case 2:
-                //spawn door with top entrance
-                break;
-            case 3:
-                //spawn door with left entrance
-                break;
-            case 4:
-                //spawn door with right entrance
-                break;
+            switch (openingDirection)
+            {
+                case 1:
+                    //spawn door with bottom entrance
+                    rand = Random.Range(0, templates.bottomRooms.Length);
+                    Instantiate(templates.bottomRooms[rand], transform.position, templates.bottomRooms[rand].transform.rotation);
+                    break;
+                case 2:
+                    //spawn door with top entrance
+                    rand = Random.Range(0, templates.topRooms.Length);
+                    Instantiate(templates.topRooms[rand], transform.position, templates.topRooms[rand].transform.rotation);
+                    break;
+                case 3:
+                    //spawn door with left entrance
+                    rand = Random.Range(0, templates.leftRooms.Length);
+                    Instantiate(templates.leftRooms[rand], transform.position, templates.leftRooms[rand].transform.rotation);
+                    break;
+                case 4:
+                    //spawn door with right entrance
+                    rand = Random.Range(0, templates.rightRooms.Length);
+                    Instantiate(templates.rightRooms[rand], transform.position, templates.rightRooms[rand].transform.rotation);
+                    break;
+
+            }
+            spawned = true;
         }
 
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Spawn Point"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
